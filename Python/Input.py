5b7e5c653463f3ea84405737dea8cf926b2fb9fe
albedo <yoware06@gmail.com>
2025-02-23 16:02:13 -0500

    Create Input.py

---
+++ Python/Input.py
@@ -0,0 +1,32 @@
+
+#name = input( " what is your name? ")
+#print(name)
+#print(name)
+
+#name = input("what is your name? ")
+#print("!"+ name + "!" + name + "!")
+
+
+#print ("hi there, " + name)
+
+#name = input("What is your name? ")
+#email = input("What is your email address? ")
+#nickname = input("What is your nickname? ")
+
+#print("Let's make sure we got this right")
+#print("Your name: " + name)
+#print("Your email address: " + email)
+#print("Your nickname: " + nickname)
+
+#address = input("What is your address? ")
+#print("So you live at address " + address)
+
+#address = input("Please type in a new address: ")
+#print("Your address is now " + address)
+
+name = input("What is your name")
+secondname = input("What is 2nd name")
+print(name + secondname)
+
+
+
