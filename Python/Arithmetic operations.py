7cd1f602757c8d9406341950b63b8674af81efad
albedo <yoware06@gmail.com>
2025-02-23 21:23:45 -0500

    Create Arithmetic operations.py

---
+++ Python/Arithmetic operations.py
@@ -0,0 +1,104 @@
+
+
+#bmi = weight / (height / 100) ** 2
+
+#print(f"The BMI is {bmi}")
+
+#x = 3
+#y = 2
+
+#print(f"/ operator {x/y}")
+#print(f"// operator {x//y
+
+#input_str = input("Which year were you born? ")
+#year = int(input_str)
+#print(f"Your age at the end of the year 2021: {2025 - year}" )
+
+#height = float(input("What is your height? "))
+#weight = float(input("What is your weight? "))
+
+#height = height / 100
+#bmi = weight / height ** 2
+
+#print(f"The BMI is {bmi}")
+
+#number1 = int(input("Type a Number"))
+#print(f"Your new number is {number1 * 5}")
+
+# input_ye = input("what year did you start")
+# year = int(input_ye)
+# print(f"you will have played for {2025 - year} years in 2025")
+
+#number1 = int(input("First number: "))
+#number2 = int(input("Second number: "))
+#number3 = int(input("Third number: "))
+
+#sum = number1 + number2 + number3
+#print(f"The sum of the numbers: {sum}")
+
+#sum = 0
+
+#sum += int(input("First number: "))
+#sum += int(input("Second number: "))
+#sum += int(input("Third number: "))
+
+#print(f"The sum of the numbers: {sum}")
+
+#number1 = int(input("First number: "))
+#number2 = int(input("Second number: "))
+
+#print(f"{number1} + {number2} = {number1+number2}
+
+
+#days = (input("How many days"))
+#time = int (days)
+#print(f"The seconds for that many days is {time * (24*60*60)}")
+
+#number1 = int(input("first"))
+#number2 = int(input("second"))
+#number3 = int(input("third"))
+#amount = str(number1 * number2 * number3)
+
+#print(f"the amount is {amount}")
+
+#number1 = int(input("first"))
+#number2 = int(input("second"))
+
+#sum = (number1 + number2)
+#product = (number1 * number2)
+#print(f"the sum is {sum}")
+#print (f"the product is {product}")
+
+#number1 = int(input("first"))
+#number2 = int(input("second"))
+#number3 = int(input("third"))
+#number4 = int(input("fourth"))
+
+#sum = 0
+
+#sum += int(input("First number: "))
+#sum += int(input("Second number: "))
+#sum += int(input("Third number: "))
+#mean = int(sum/4)
+
+#print(f"The sum of the numbers:",sum,"and the mean is", mean)
+
+
+
+#typical = float(input("How many times do you eat at cafe a week"))
+#average = float(input("Average food price at cafe "))
+#weeklyt = float(input("How much do you spend a week on grocery?"))
+
+#weekly = float (typical * average + weeklyt)
+#daily = float(weekly/7)
+
+#print(f"Daily: {daily}")
+#print(f"Weekly: {weekly}")
+
+People = int(input("how many people on the paper"))
+group = int(input("how many people per paper group"))
+
+amount = int (People // group )
+
+print(f"number of groups formed {amount}")
+
