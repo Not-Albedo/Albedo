8b1edfb94aa9a5d457eda811c79d1e4b5235a826
albedo <yoware06@gmail.com>
2025-02-25 22:33:44 -0500

    Create Conditional statements.py

---
+++ Python/Conditional statements.py
@@ -0,0 +1,62 @@
+#age = int(input("your age"))
+
+#if age > 17:
+    #print("you may buy")
+    #print("here is game")
+
+#print("move along")
+
+#number = int(input("Please type in a number: "))
+
+#if number < 0:
+    #print("The number is negative.")
+
+#if number > 0:
+    #print("The number is positive.")
+
+#if number == 0:
+    #print("The number is zero.")
+
+#password = input("insert password: \n")
+
+#if password == "flag":
+    #print("access")
+
+#print("finished")
+
+#number = int(input('please type number in \n'))
+
+#if number == 1939:
+    #print("welcome")
+
+#coin = int(input('insert coin'))
+
+#if coin < 0:
+    #print(f"the number is now", (coin * -1))
+
+#money = int(input("put amount"))
+#if money <= 1000 or money > 500:
+    #print("less than 1000")
+#if money < 500:
+    #print("less than 500")
+#if money < 100:
+    #print("less than 100")
+#if money >= 1000:
+    #print("entered")
+
+
+#a = 3
+#condition = a < 5
+#print(condition)
+#if condition:
+    #print("a is less than 5")
+
+#number1 = int(input("insert number 1"))
+#number2 = int(input("insert number 2"))
+#print( number1 + number2)
+
+card = int(input("how many points?"))
+if card < 100:
+    print(f"your bonus is 10% \n",(card * .10 + card))
+if card >= 100:
+    print(f" your bonus is 15% \n", "you now have",(card * .15 +card))
