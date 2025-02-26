faf7ddaa32820f2ff995bd9f43d7007b8e5cb763
albedo <yoware06@gmail.com>
2025-02-25 23:15:21 -0500

    Create conditional 2.py

---
+++ Python/conditional 2.py
@@ -0,0 +1,20 @@
+#number = int(input("Please type in a number: "))
+#number2 = 0
+#if number > int(100):
+    #print("The number was greater than one hundred")
+    #int(number - 100 + number2)
+    #print("Now its value has decreased by one hundred")
+    #print("Its value is now", int(number - 100 + number2))
+#if number < int(100):
+    #print(number," must be my lucky number!")
+    #print("Have a nice day!")
+
+#keyword = input("type a word ")
+#print("there are",len(keyword), "letters in",keyword)
+
+coin = float(input("insert word"))
+
+print(int(coin))
+decimal = coin - int(coin)
+print(decimal)
+
