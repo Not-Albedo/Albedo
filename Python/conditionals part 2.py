4fa9d6fa5d3d6009178b901323ab8f90fda33fd1
albedo <yoware06@gmail.com>
2025-02-25 23:29:39 -0500

    Create conditionals part 2.py

---
+++ Python/conditionals part 2.py
@@ -0,0 +1,31 @@
+#number = int(input("Please type in a number: "))
+
+#if number % 2 == 0:
+#    print("The number is even")
+#else:
+#    print("The number is odd")
+#correct = "kittycat"
+#password = input("Please type in the password: ")
+
+#if password == correct:
+#    print("Welcome")
+#else:
+#    print("No admittance")
+#goals_home = int(input("Home goals scored: "))
+#goals_away = int(input("Away goals scored: "))
+
+#if goals_home > goals_away:
+#    print("The home team won!")
+#elif goals_away > goals_home:
+#    print("The away team won!")
+#else:
+#    print("It's a tie!")
+
+number1 = int(input("type number:"))
+number2 = int(input("type number:"))
+
+if number1 > number2:
+    print(number1,"is greater")
+elif number2 > number1:
+    print(number1,"is greater")
+
