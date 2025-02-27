a05afb131850d5c8f63b7860c87c1724f964d339
albedo <yoware06@gmail.com>
2025-02-26 21:14:21 -0500

    Create Combining conditions.py

---
+++ Python/Combining conditions.py
@@ -0,0 +1,106 @@
+#number = int(input("Please type in a number: "))
+#if number >= 5 and number <= 8:
+    #print("The number is between 5 and 8")
+#elif number < 5 or number > 8:
+   # print("The number is not within the range of 5 to 8")
+#n1 = int(input("Number 1: "))
+#n2 = int(input("Number 2: "))
+#n3 = int(input("Number 3: "))
+#n4 = int(input("Number 4: "))
+
+#if n1 > n2 and n1 > n3 and n1 > n4:
+#    greatest = n1
+#elif n2 > n3 and n2 > n4:
+#    greatest = n2
+#elif n3 > n4:
+#    greatest = n3
+#else:
+ #   greatest = n4
+
+#print(f" {greatest} is the greatest of the numbers.")
+
+#age = int(input("type level"))
+#if age >= 8 and age <= 20:
+#    print("you are the right level to enter")
+#elif age < 0:
+#    print("you are a bug")
+#elif not age >=8 and age <=20:
+    #print("you are not high enough level")
+
+#name = input("please type your name ")
+#if name == "Mike":
+#    print("Welcome Red Army")
+#elif name == "Redge":
+#    print("Welcome Red Army")
+#elif name == "Charles":
+#    print("Welcome Black Army")
+#elif name == "Deck":
+#    print("Welcome Black Army")
+#elif name:
+   # print("You are Denied")
+
+#points = int(input("Enter Your Score"))
+#if points > 100:
+#    print("Prodigy")
+#elif points >= 90 or points > 100:
+#    print("Genius")
+#if 75 <= points <= 89:
+#    print("Smart")
+#if 1 <= points <= 74:
+ #   print("lol")
+
+#number = int(input("please type in a number"))
+#if number%5==0:
+#    print("5 Shattered Pieces")
+#elif number%3==0:
+#    print("3 Shattered Pieces")
+#elif number%2==0:
+#    print("Split in Two Piece")
+
+#number = int(input("Please type in a number: "))
+
+#if number > 0:
+#    if number % 2 == 0:
+#        print("The number is even")
+#    else:
+#        print("The number is odd")
+#else:
+   # print("The number is negative or zero")
+
+# same example as above but different way to right it
+
+#number = int(input("Please type in a number: "))
+
+#if number > 0 and number % 2 == 0:
+#    print("The number is even")
+#elif number > 0 and number % 2 != 0:
+#    print("The number is odd")
+#else:
+#    print("The number is negative or zero")
+
+#year = int(input("Type the Year"))
+
+#if year % 4==0 and year % 100!=0 or year%400==0:
+#    print("is leap year")
+
+#letter1 = input("1st letter")
+#letter2 = input("2nd letter")
+#letter3 = input("3rd letter")
+
+#letters = [letter1, letter2, letter3]
+#letters.sort()
+#print(f"the middle letter is: {letters[1]}")
+
+#gift = float(input("please type gift amount"))
+
+#if 4999 >= gift:
+#    print("No Tax")
+#elif 5000 <= gift <= 25000:
+#   print(float(100 + (gift - 5000)* 0.08))
+#elif 25001<= gift <= 55000:
+#    print(float(1700+(gift - 5000)*0.10))
+#elif 55001 <= gift <= 80000:
+ #   print(float(4700+(gift-55000)*0.12))
+
+
+
